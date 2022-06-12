568e03b825feccf827046a350e5cf35bd7cd4e95
unknown <godsonemmanuel07@gmail.com>
2022-06-12 11:02:13 +0530

    Setting up server to fetch project and client details using GraphQL in backend

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1>Client Projects</h1>
+Manage Client and their projects.
---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const colors = require('colors');
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
+
+var app = express();
+
+app.use(express.json());
+
+// Connect to database
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
